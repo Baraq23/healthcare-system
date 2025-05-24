29076bca9fc3cbd436363024e4fa6adfa6e42a81
barraotieno <barrackotieno160@gmail.com>
2025-05-25 00:22:33 +0300

    refactor: change relative package importing to absolute importing (remove dots on import statements).

--- database.py
+++ database.py
@@ -1,9 +1,30 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mysecurepassword@localhost/healthcare"
+load_dotenv()  # Loads variables from a .env file if present
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+
+# instead of: SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost/healthcare"
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+# Dependency to get database sessions
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  # Provide session to route
+    finally:
+        db.close()  # Close after request completes
