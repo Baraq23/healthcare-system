6d52c7d5826aaa62bce457c27edc88735c311888
barraotieno <barrackotieno160@gmail.com>
2025-05-26 01:26:52 +0300

    chore: introduce specialization relationship to doctors model table.

--- models/doctor.py
+++ models/doctor.py
@@ -1,6 +1,10 @@
-from sqlalchemy import Column, Integer, String, Date, DateTime
+from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from database import SessionLocal, Base  # Direct import
+from database import Base
+
+
+
 
 class Doctor(Base):
     __tablename__ = "doctors"
@@ -10,7 +14,10 @@ class Doctor(Base):
     last_name = Column(String(50), nullable=False)
     date_of_birth = Column(Date, nullable=False)
     gender = Column(String(10), nullable=False)
+    specialization_id = Column(Integer, ForeignKey("specializations.id"), nullable=False)
+    specialization = relationship("Specialization", back_populates="doctors")  # Many-to-one
     email = Column(String(100), unique=True, nullable=False)
     phone = Column(String(20), nullable=False)
     address = Column(String(200), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
