1e4c7140a485cdfd631f01b4ec3feff749e84fbb
barraotieno <barrackotieno160@gmail.com>
2025-05-25 22:06:08 +0300

    chore: include doctor routers in main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from database import Base, engine  
-from routers import patients  
+from routers import patient, doctor
 
 # Creating tables before creating FastAPI app
 Base.metadata.create_all(bind=engine)
 
+# creating FastAPI app instance
 app = FastAPI()
 
-# creating FastAPI app instance
-app.include_router(patients.router)
+app.include_router(patient.router)
+app.include_router(doctor.router)
 
 @app.get("/")
 def root():
