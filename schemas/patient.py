253d94399ca1e44295c00e54ff625144b126386a
barraotieno <barrackotieno160@gmail.com>
2025-05-25 14:21:42 +0300

    chore: introduce gender validation for the gender field for patients to accept 'male' and 'female' in whichever letter cases (case-insensitivity).

--- schemas/patient.py
+++ schemas/patient.py
@@ -1,15 +1,37 @@
-from datetime import datetime
-from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime, date
+from pydantic import BaseModel, EmailStr, Field, field_validator
+from typing import Optional
+from enum import Enum
+
+
+
+# validations classes
+class Gender(str, Enum):
+    MALE = "male"
+    FEMALE = "female"
+    
+
+
+
+
+
 
 # Base schema
 class PatientBase(BaseModel):
-    first_name: str = Field(..., min_length=3, max_length=25, description='First name of the patient')
-    last_name: str = Field(..., min_length=3, max_length=25, description='Last name of the patient')
-    date_of_birth: datetime = Field(...,  description='Patient date of birth')
-    gender: str = Field(...,  description='Patient date gender')
-    email: EmailStr = Field(None,  description='Patient email address')
+    first_name: str = Field(..., min_length=3, max_length=50, description='First name of the patient')
+    last_name: str = Field(..., min_length=3, max_length=50, description='Last name of the patient')
+    date_of_birth: date = Field(...,  description='Patient date of birth')
+    gender: Gender = Field(...,  description='Patient gender')
+    email: EmailStr = Field(...,  description='Patient email address')
     phone: str = Field(...,  description='Patient phone number')
-    address: str | None = None
+    address: Optional[str] = None
+
+    @field_validator('gender', mode='before')
+    @classmethod
+    def normalize_gender(cls, v):
+        if isinstance(v, str):
+            return v.lower()
+        return v
 
 # Schema for creating new patients (POST requests)
 class PatientCreate(PatientBase):
@@ -17,11 +39,11 @@ class PatientCreate(PatientBase):
 
 # Schema for updating patients (PUT/PATCH requests)
 class PatientUpdate(BaseModel):
-    first_name: str | None = None
-    last_name: str | None = None
-    email: EmailStr | None = None
-    phone: str | None = None
-    address: str | None = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    phone: Optional[str] = None
+    address: Optional[str] = None
 
 # Schema for returning patients (GET responses)
 class Patient(PatientBase):
