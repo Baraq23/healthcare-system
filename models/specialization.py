e6087a5809398dbdcda3a6f66fb12e0fcf0dfaf4
barraotieno <barrackotieno160@gmail.com>
2025-05-26 01:16:00 +0300

    feat: add specialization model.

---
+++ models/specialization.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Specialization(Base):
+    __tablename__ = "specializations"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+    doctors = relationship("Doctor", back_populates="specialization")  # One-to-many
