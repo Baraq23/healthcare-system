b975dbac774a698db67033501a50c98aa543b8de
barraotieno <barrackotieno160@gmail.com>
2025-06-01 16:12:52 +0300

    feat: reserve adding apecialization to admins.

--- routers/specialization.py
+++ routers/specialization.py
@@ -1,45 +1,36 @@
-# routers/specializations.py
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
 from models.specialization import Specialization as SpecializationModel
-from schemas.specialization import SpecializationCreate as SpecializationCreateSchema
+# from schemas.specialization import SpecializationCreate as SpecializationCreateSchema
 from schemas.specialization import Specialization as SpecializationSchema
-
-
-
-from utils.helper import (
-    specialization_exists_by_name,
-    get_specialization_by_id_or_404,
-)
-
+from utils.helper import get_specialization_by_id_or_404
 from models.specialization import Specialization as SpecializationModel
-from schemas.specialization import (
-    SpecializationCreate as SpecializationCreateSchema,
-    Specialization as SpecializationSchema,
-)
+from schemas.specialization import Specialization as SpecializationSchema
 
 router = APIRouter(prefix="/specializations", tags=["specializations"])
 
-@router.post("/", response_model=SpecializationSchema)
-def create_specialization(
-    specialization: SpecializationCreateSchema,
-    db: Session = Depends(get_db)
-):
-    """Create a new specialization."""
-    if specialization_exists_by_name(db, specialization.name):
-        raise HTTPException(
-            status_code=400,
-            detail="Specialization already exists"
-        )
-    # capitalize specialization name
-    specialization_data = specialization.model_dump(exclude={"sapecialization_name"})
-    specialization_data["specialization_name"] = specialization.name[0].upper() + specialization.name[1:].lower()
-    db_spec = SpecializationModel(**specialization.model_dump())
-    db.add(db_spec)
-    db.commit()
-    db.refresh(db_spec)
-    return db_spec
+
+#CREATE SPCIALIZATION IS PRESERVED FOR ADMINS
+# @router.post("/", response_model=SpecializationSchema)
+# def create_specialization(
+#     specialization: SpecializationCreateSchema,
+#     db: Session = Depends(get_db)
+# ):
+#     """Create a new specialization."""
+#     if specialization_exists_by_name(db, specialization.name):
+#         raise HTTPException(
+#             status_code=400,
+#             detail="Specialization already exists"
+#         )
+#     # capitalize specialization name
+#     specialization_data = specialization.model_dump(exclude={"sapecialization_name"})
+#     specialization_data["specialization_name"] = specialization.name[0].upper() + specialization.name[1:].lower()
+#     db_spec = SpecializationModel(**specialization.model_dump())
+#     db.add(db_spec)
+#     db.commit()
+#     db.refresh(db_spec)
+#     return db_spec
 
 @router.get("/", response_model=list[SpecializationSchema])
 def get_all_specializations(
@@ -47,7 +38,7 @@ def get_all_specializations(
     limit: int = 100,
     db: Session = Depends(get_db)
 ):
-    """Get all specializations with pagination."""
+    """Get all specializations with pagination. Adding specialization is reserved for admins"""
     return db.query(SpecializationModel).order_by(SpecializationModel.name.asc()).offset(skip).limit(limit).all()
 
 @router.get("/{specialization_id}", response_model=SpecializationSchema)
