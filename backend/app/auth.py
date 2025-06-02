451b9b6be6e6e50d6e7a3850e6b11b198ecc7ab9
barraotieno <barrackotieno160@gmail.com>
2025-06-02 12:58:32 +0300

    update environment variables

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -19,7 +19,7 @@ import os
 # JWT settings 
 SECRET_KEY = os.getenv("SECRET_KEY","healthcare123") 
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
 
 
 # Configure logging
